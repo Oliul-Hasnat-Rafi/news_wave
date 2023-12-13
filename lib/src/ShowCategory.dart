99ebcc2a92f7c3b939e7933fa71eb8a59716aec0
Oliul-Hasnat-Rafi <oliulhasnatrafi270@gmail.com>
2023-12-13 23:02:40 +0600

    cat

--- lib/src/ShowCategory.dart
+++ lib/src/ShowCategory.dart
@@ -22,13 +22,62 @@ class _ShowCategoryState extends State<ShowCategory> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          widget.categoryname.toString(),
-          style: TextStyle(
-              color: Colors.black, fontSize: 26.0, fontWeight: FontWeight.bold),
+        appBar: AppBar(
+          title: Text(
+            widget.categoryname.toString(),
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10.0),
+          child: ListView.builder(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              itemCount: catarticles.Categorydata!.articles!.length,
+              itemBuilder: (context, index) {
+                return catarticles.Categorydata!.articles!.length == 0
+                    ? CircularProgressIndicator()
+                    : Container(
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.network(
+                                    catarticles.Categorydata!.articles![index]
+                                        .urlToImage
+                                        .toString(),
+                                    width: MediaQuery.of(context).size.width,
+                                    height: 200,
+                                    fit: BoxFit.cover)),
+                            SizedBox(
+                              height: 5.0,
+                            ),
+                            Text(
+                              catarticles.Categorydata!.articles![index].title
+                                  .toString(),
+                              maxLines: 2,
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              catarticles
+                                  .Categorydata!.articles![index].description
+                                  .toString(),
+                              maxLines: 3,
+                            ),
+                            SizedBox(
+                              height: 20.0,
+                            ),
+                          ],
+                        ),
+                      );
+              }),
+        ));
   }
 }
